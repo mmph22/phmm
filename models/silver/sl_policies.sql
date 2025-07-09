89202c6e91dd9432c3ea49a5526aa44d76d8d246
Hythabada Prashanthi <phythabada@gmail.com>
2025-07-09 09:43:19 +0000

    commit

--- models/silver/sl_policies.sql
+++ models/silver/sl_policies.sql
@@ -1,11 +1,23 @@
 {{ config(
     materialized='table',
-    pre_hook=[
-        "{{ insert_data_into_audit_table('INS', model.name, 'raw', none, 'SL_LOAD_START', 'sl', 'SAP', 'Y') }}"
-    ],
-    post_hook=[
-        "{{ insert_data_into_audit_table('UPD', model.name, 'raw', 'SUCCESS', 'SL_LOAD_COMPLETE', 'sl', '', '') }}"
-    ]
+    pre_hook="{% set status = insert_data_into_audit_table(
+        model.config.ops_ins,
+        model.name,
+        model.config.src_name,
+        model.config.status_start,
+        model.config.proc_typ_msg_start,
+        model.config.stage,
+        model.config.integration_id,
+        model.config.flag
+    ) %}",post_hook=["{% set status = insert_data_into_audit_table(
+        model.config.ops_upd,
+        model.name,
+        model.config.src_name,      
+        model.config.status_success,
+        model.config.proc_typ_msg_success,
+        model.config.stage,
+        model.config.integration_id,
+        '') %}"]
 ) }}
 
 select
