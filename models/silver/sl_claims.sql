a61965fecf3e5caaf91d4fa666fff7502165bbad
Hythabada Prashanthi <phythabada@gmail.com>
2025-07-09 14:44:19 +0000

    test

--- models/silver/sl_claims.sql
+++ models/silver/sl_claims.sql
@@ -9,18 +9,41 @@
         model.config.stage,
         model.config.integration_id,
         model.config.flag
-    ) %}",post_hook=["{% set status = insert_data_into_audit_table(
-        model.config.ops_upd,
-        model.name,
-        model.config.src_name,      
-        model.config.status_success,
-        model.config.proc_typ_msg_success,
-        model.config.stage,
-        model.config.integration_id,
-        '') %}"]
+    ) %}",
+    post_hook=[
+        "{% set status = insert_data_into_audit_table(
+            model.config.ops_upd,
+            model.name,
+            model.config.src_name,
+            model.config.status_success,
+            model.config.proc_typ_msg_success,
+            model.config.stage,
+            model.config.integration_id,
+            '') %}",
+
+        "{{ insert_record_count_recon(
+            model.name,
+            'CLAIMS',
+            '{}.{}_{}.{}'.format(
+                env_var('DBT_AUDIT_DB'),
+                'dbt_ph',
+                env_var('DBT_SL_SCHEMA'),
+                model.name
+            ),
+            '{}.{}_{}.{}'.format(
+                env_var('DBT_AUDIT_DB'),
+                'dbt_ph',
+                env_var('DBT_RAW_SCHEMA'),
+                'RAW_CLAIMS'
+            ),
+            {'base': 'CLAIM_STATUS = ''Approved'''}, 
+            {'base': 1, 'target': 1}
+        ) }}"
+    ]
 ) }}
 
-{% set pk_column = 'claim_id' %}  
+
+{% set pk_column = 'claim_id' %}
 {% set raw_table = ref('raw_claims') %}
 
 with filtered as (
