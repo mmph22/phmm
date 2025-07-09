f81e84f38d1f0835a955f41b375dbd475e2529d0
Hythabada Prashanthi <phythabada@gmail.com>
2025-07-09 05:21:55 +0000

    test

--- models/silver/project_failed_rec.sql
+++ models/silver/project_failed_rec.sql
@@ -3,17 +3,16 @@
         materialized='table'
     )
 }}
-{% set source_model = ref('raw', 'claims')' %}
+{% set source_model = ref ('raw', 'project')' %}
 {% set target_model = 'stg_valid_customers' %}
-{% set error_table  = 'INSURANCE_DB.AUDIT_SCHEMA.
-FAILED_TEST_RECORDS' %}
+{% set error_table  = 'INSURANCE_DB.
+DBT_PH_STAGING' %}
 {% set unique_check_columns = ['project_id'] %}
 
 {{ validate_and_route(
     source_model=source_model,
     target_model=target_model,
     error_table=error_table,
-    null_check_columns=null_check_columns,
     unique_check_columns=unique_check_columns
 ) }}
 
