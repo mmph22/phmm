0c4a3e9c6a1bb700e945ac79ff09115199be5c3e
Hythabada Prashanthi <phythabada@gmail.com>
2025-07-10 10:45:13 +0000

    commit-v

--- models/raw/raw_claims.sql
+++ models/raw/raw_claims.sql
@@ -1,11 +1,17 @@
--- SELECT * FROM {{ source('raw', 'claims') }}
-
 {{ 
   config( 
     materialized='incremental', 
-    unique_key='claim_id'  
   ) 
 }}
 
-SELECT *, {{ incremental_hash('claims') }} 
+SELECT 
+    {% if is_incremental() %}
+      (select coalesce(max(batch_id) + 1, 1) from {{ this }}) batch_id
+    {% else %}
+      1 as batch_id
+    {% endif %}
+    , * 
 FROM {{ source('raw', 'claims') }}
+{% if is_incremental() %}
+    where last_updated > (select coalesce(max(last_updated), '1900-01-01') from {{ this }})
+{% endif %}
--- models/silver/sl_claims.sql
+++ models/silver/sl_claims.sql
@@ -1,17 +1,26 @@
+{% set pk_column = 'claim_id' %}
+{% set raw_table = ref('raw_claims') %}
+{% set batch_filter = "batch_id = (select max(batch_id) from " ~ this ~ ")" %}
+
 {{ config(
-    materialized='table',
-    pre_hook="{% set status = insert_data_into_audit_table(
-        model.config.ops_ins,
-        model.name,
-        model.config.src_name,
-        model.config.status_start,
-        model.config.proc_typ_msg_start,
-        model.config.stage,
-        model.config.integration_id,
-        model.config.flag
-    ) %}",
+    materialized='incremental',
+    on_schema_change='sync_all_columns',
+
+    pre_hook=[
+        "{{ insert_data_into_audit_table(
+            model.config.ops_ins,
+            model.name,
+            model.config.src_name,
+            model.config.status_start,
+            model.config.proc_typ_msg_start,
+            model.config.stage,
+            model.config.integration_id,
+            model.config.flag
+        ) }}"
+    ],
+
     post_hook=[
-        "{% set status = insert_data_into_audit_table(
+        "{{ insert_data_into_audit_table(
             model.config.ops_upd,
             model.name,
             model.config.src_name,
@@ -19,35 +28,42 @@
             model.config.proc_typ_msg_success,
             model.config.stage,
             model.config.integration_id,
-            '') %}",
+            ''
+        ) }}",
 
         "{{ insert_record_count_recon(
             model.name,
             'CLAIMS',
-            '{}.{}_{}.{}'.format(
-                env_var('DBT_AUDIT_DB'),
-                'dbt_ph',
-                env_var('DBT_SL_SCHEMA'),
-                model.name
-            ),
-            '{}.{}_{}.{}'.format(
-                env_var('DBT_AUDIT_DB'),
-                'dbt_ph',
-                env_var('DBT_RAW_SCHEMA'),
-                'RAW_CLAIMS'
-            ),
-            {'base': 'CLAIM_STATUS = ''Approved'''}, 
-            {'base': 1, 'target': 1}
+            '{}.{}_{}.{}'.format(env_var('DBT_AUDIT_DB'), 'DBT_PH', env_var('DBT_SL_SCHEMA'), model.name),
+            '{}.{}_{}.{}'.format(env_var('DBT_AUDIT_DB'), 'DBT_PH', env_var('DBT_RAW_SCHEMA'), 'RAW_CLAIMS'),
+            where_condition={
+                'base': batch_filter,
+                'target': batch_filter
+            }
         ) }}"
     ]
 ) }}
 
 
-{% set pk_column = 'claim_id' %}
-{% set raw_table = ref('raw_claims') %}
+with max_batch as (
+    {% if is_incremental() %}
+        select coalesce(max(batch_id), 1) as batch_id_value from {{ this }}
+    {% else %}
+        select 1 as batch_id_value
+    {% endif %}
+),
 
-with filtered as (
+filtered as (
     {{ exclude_failed_rows(raw_table, pk_column) }}
+),
+
+with_batch_id as (
+    select f.*
+    from filtered f
+    cross join max_batch m
+    {% if is_incremental() %}
+        where f.batch_id > m.batch_id_value
+    {% endif %}
 )
 
-select * from filtered
+select * from with_batch_id
