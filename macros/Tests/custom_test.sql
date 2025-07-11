ecd5c6e9416af0dd4e844247a9c5cefdcbc18cdd
Hythabada Prashanthi <phythabada@gmail.com>
2025-07-11 02:57:09 +0000

    C

--- macros/Tests/custom_test.sql
+++ macros/Tests/custom_test.sql
@@ -3,7 +3,10 @@
 {% endtest %}
 
 {% test assert_column_unique(model, column_name) %}
-    SELECT {{ column_name }} FROM {{ model }} GROUP BY {{ column_name }} HAVING COUNT(*) > 1
+    SELECT * FROM {{ model }} M
+    WHERE {{ column_name }} IN 
+            (SELECT {{ column_name }} FROM {{ model }} 
+                GROUP BY {{ column_name }} HAVING COUNT(*) > 1)
 {% endtest %}
 
 {% test expect_column_values_to_be_in_list(model, column_name, values) %}
@@ -14,11 +17,12 @@
 
 
 {% test validate_foreign_key(model, column_name, ref_model, ref_column) %}
-    SELECT {{ column_name }}
-    FROM {{ model }} m
-    WHERE NOT EXISTS(
-        SELECT 1
-        FROM {{ ref_model }} rm
-        WHERE rm.{{ ref_column }}= m.{{ column_name }}
-    )
+    SELECT * FROM {{ model }} M
+    WHERE {{ column_name }} IN (SELECT {{ column_name }}
+                                    FROM {{ model }} m
+                                    WHERE NOT EXISTS(
+                                        SELECT 1
+                                        FROM {{ ref_model }} rm
+                                        WHERE rm.{{ ref_column }}= m.{{ column_name }}
+                                    ))
 {% endtest %}
